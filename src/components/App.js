055cc1ac17a583a0c5be2fee2204c18f764d53a1
jchen <janetchen906@gmail.com>
2020-06-06 10:03:03 -0700

    skeleton component display and save col

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Login from './UserForms/Login';
 import Registration from './UserForms/Registration';
+import SaveColumn from './SaveColumn/SaveColumn';
+import Display from './Display/Display';
 
 function App() {
   return (
     <div>
       <Login />
       <Registration />
+      <Display />
+      <SaveColumn />
     </div>
   );
 }
---
+++ src/components/SaveColumn/SaveColumn.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './SaveColumn.css'
+
+export default class Registration extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div className="saveColumn">
+                Your Favourites
+            </div>
+        )
+    }
+}
