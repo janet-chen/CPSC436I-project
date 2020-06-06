b0e4ed6fcc8ceaba293a70d375e4e721462127b5
kevinhryang <32024558+kevinhryang@users.noreply.github.com>
2020-06-06 13:29:30 -0700

    Started simple searchbar and calendar (#6)
    Co-authored-by: khyang <khyang@students.cs.ubc.ca>
    Co-authored-by: Janet Chen <17347608+janet-chen@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css';
 import Login from './UserForms/Login';
 import Registration from './UserForms/Registration';
+import MyCalendar from './Calendar';
+import Searchbar from './Searchbar';
 import SaveColumn from './SaveColumn/SaveColumn';
 import Display from './Display/Display';
+
 
 function App() {
   return (
     <div>
       <Login />
       <Registration />
+      <Searchbar />
       <Display />
       <SaveColumn />
+      <MyCalendar />
     </div>
   );
 }
