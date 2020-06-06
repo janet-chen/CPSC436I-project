5ff92f79b9f6ac260a4a80276e64440f478858ef
qstevens <32005151+qstevens@users.noreply.github.com>
2020-06-06 10:32:18 -0700

    integrate react router (#3)

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import './App.css';
 import Login from './UserForms/Login';
 import Registration from './UserForms/Registration';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Login />
-      <Registration />
-    </div>
+    <Router>
+      {/* Replace this with nav */}
+      <Link to="/" >
+        <h3>Home</h3>
+      </Link>
+      <Link to="/login" >
+        <h3>Login</h3>
+      </Link>
+      <Link to="/register" >
+        <h3>Register</h3>
+      </Link>
+
+      <Switch>
+        <Route path="/" component={null} exact />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Registration} />
+      </Switch>
+
+    </Router>
   );
 }
 
