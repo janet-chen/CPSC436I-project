0f54542f0c9fc4a5523c2edd61aa538b880690c1
ckvu <chris_tine_vu_654@hotmail.com>
2020-06-18 17:59:23 -0700

    Incorporate photo query functionality (#11)
    Co-authored-by: Christine Vu <christine.vu@paihealth.com>

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,64 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './Display.css'
+import './Display.css';
 import Media from '../Media/Media';
 
 class Display extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-        };
-    }
+  constructor (props) {
+    super(props);
 
-    render() {
-        return (
-            <div className="display">
+    this.state = {
+    };
+  }
+
+  render () {
+    return (
+      <div className='display'>
                 Loaded content... Here's your query: {this.props.query}
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-                <Media />
-            </div>
-        )
-    }
+        {this.props.results.map((imgInState, id) => {
+          return <img key={id} src={imgInState.urls.small} alt='An image' />;
+        })}
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+        <Media />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
   return {
-    query: state.submittedSearch.currentSearch
+    query: state.submittedSearch.currentSearch,
+    results: state.submittedImages.savedImages
   };
 };
 
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,33 +1,58 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import setSearch from '../actions/index';
+import { setSearch, saveResults } from '../actions/index';
+import Unsplash, { toJson } from 'unsplash-js';
+const APP_ACCESS_KEY = 'adYDaJCisGClWz_PnSuJJiuzl1hItt3kKGxKLePeYPA';
+const SECRET_KEY = 'KFaS1rVbg4l9G1OJ1TSVCL2mYnDMl9c-5wi8puO2Bjo';
+const unsplash = new Unsplash(
+  {
+    accessKey: APP_ACCESS_KEY,
+    secret: SECRET_KEY
+  });
 
 class Searchbar extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      value: '',
+      image: null
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event) {
+  handleSubmit (event) {
     this.props.submitSearch(this.state.value);
-    console.log("Search: " + this.state.value);
+    unsplash.search.photos(this.state.value, 1, 10, { orientation: 'portrait' })
+      .then(toJson)
+      .then(json => {
+        let extractUrls = function ({ urls }) { return { urls }; };
+        const imageSubset = json.results.slice(0, 10);
+        const urls = imageSubset.map(imgObject => extractUrls(imgObject));
+        this.props.submitResults(urls);
+        this.setState({
+          image: json.results[0].urls.small
+        });
+      });
     event.preventDefault();
   }
 
-  handleChange(event) {
+  handleChange (event) {
     this.setState({
       value: event.target.value
-    })
+    });
   }
 
-  render() {
+  render () {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input id="submitText" type="text" value={this.state.value} onChange={this.handleChange}/>
-        <button onClick={() => this.handleSubmit}>Search</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input id='submitText' type='text' value={this.state.value} onChange={this.handleChange} />
+          <button onClick={() => this.handleSubmit}>Search</button>
+        </form>
+        <img src={this.state.image} alt='Destination Image' />
+      </div>
+
     );
   }
 }
@@ -36,6 +61,9 @@ const mapDispatchToProps = (dispatch) => {
   return {
     submitSearch: (destination) => {
       dispatch(setSearch(destination));
+    },
+    submitResults: (imageUrls) => {
+      dispatch(saveResults(imageUrls));
     }
   };
 };
