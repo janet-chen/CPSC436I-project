8f0bea61c156ec738f4798366c089ed06c2c09d4
jchen <janetchen906@gmail.com>
2020-07-09 15:11:46 -0700

    get and post api set up

--- api/app.js
+++ api/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,6 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var favouritesRouter = require('./routes/favourites');
 
 var app = express();
 
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/favourites', favouritesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ api/routes/favourites.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+
+// gets URI from .env 
+var uri = process.env.DB_URI;
+var MongoClient = require('mongodb').MongoClient;
+
+/* GET favourites listing. */
+router.get('/', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    MongoClient.connect(uri, function (err, client) {
+        if (err) {
+            console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+        }
+        console.log('Connected...');
+        const collection = client.db("travelr").collection("favourites");
+        collection.find({}).toArray((err, result) => {
+            if (err) console.log(err)
+            else {
+                res.send(result);
+            }
+        })
+        // perform actions on the collection object
+        client.close();
+    });
+});
+
+
+/* POST a favourite */
+router.post('/', function(req, res, next) {
+    const newFavourite = req.body;
+    // newMessage.id = uuid();
+    MongoClient.connect(uri, function (err, client) {
+        if (err) {
+            console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+        }
+        console.log('Connected...');
+        const collection = client.db("travelr").collection("favourites");
+        collection.insertOne(
+            newFavourite
+        )
+        // perform actions on the collection object
+        client.close();
+    });
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(newFavourite);
+});
+
+module.exports = router;
