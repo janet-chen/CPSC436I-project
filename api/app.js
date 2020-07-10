459f7dbc3559a3f375787c55c751d75f6e09144e
Christine Vu <christine.vu@paihealth.com>
2020-07-10 02:35:05 -0700

    Implement video querying backend

--- api/app.js
+++ api/app.js
@@ -10,6 +10,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var favouritesRouter = require('./routes/favourites');
+var videosRouter = require('./routes/videos');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/favourites', favouritesRouter);
+app.use('/findVideos', videosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
