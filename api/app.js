415d6bbb1d5346c8e84d0ff3278b22999bd09772
Janet Chen <17347608+janet-chen@users.noreply.github.com>
2020-07-24 20:51:20 -0700

    login with google oauth (#35)
    * adding oauth passport
    
    * auth login
    
    * removed config keys

--- api/app.js
+++ api/app.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+var passportSetup = require('./config/passports-setup');
+var mongoose = require('mongoose');
+var cookieSession = require('cookie-session');
+var passport = require('passport');
 
 var createError = require('http-errors');
 var express = require('express');
@@ -11,12 +15,24 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var favouritesRouter = require('./routes/favourites');
 var videosRouter = require('./routes/videos');
+var authRoutes = require('./routes/auth');
+
+var dbURI = process.env.DB_URI;
+var cookieKey = process.env.cookieKey;
 
 var app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// connect to mongodb
+mongoose.connect(dbURI, () => {
+  console.log('connected to mongodb');
+})
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,6 +45,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/favourites', favouritesRouter);
 app.use('/findVideos', videosRouter);
+app.use('/auth', authRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,5 +62,15 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(cookieSession({
+    // one day in milliseconds
+    maxAge: 24 * 60 * 60 * 1000,
+    // encrypt cookie id key
+    keys: [cookieKey]
+}))
+
+// initialize passport
+
 
 module.exports = app;
